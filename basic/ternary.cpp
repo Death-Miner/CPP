33041ea4e18180bff70f7138998e34d567f962cc
Vishal Singh Gaira <vishalsinghgaira01@gmail.com>
2026-04-21 13:04:10 +0530

    Create ternary.cpp

---
+++ basic/ternary.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int grade;
+    cout<<"Enter the grade of the student  "<<endl;
+    cin>>grade;
+    
+    grade>60 ? cout<<"The Studnt is passed":cout<<"Failed!!!";
+    cout<<"HI;";
+    return 0;
+}
