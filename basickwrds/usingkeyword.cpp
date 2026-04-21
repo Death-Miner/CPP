5573cd1a68757864cad7860411df5bf73bd64a53
Vishal Singh Gaira <vishalsinghgaira01@gmail.com>
2026-04-21 11:47:34 +0530

    Create usingkeyword.cpp

---
+++ basickwrds/usingkeyword.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// typedef vector<int> vecint;
+// typedef vector<string> vecstr;
+// typedef vector<float> vecflt;
+using vecint= vector<int>;
+using vecflt= vector<float>;
+using vecstr= vector<string>;
+
+
+int main() {
+    vecint v1={34, 345, 245, 24532};
+    vecstr v2={"ch", "char", "Str"};
+    vecflt v3={34.24, 34.3, 3211.4};
+
+    vecint::iterator itr;
+
+    for (itr=v1.begin(); itr!=v1.end(); itr++) {
+        cout<<*itr<<" ";
+    }
+
+    cout<<endl;
+
+    return 0;
+}
