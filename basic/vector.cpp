ba85cffa097137cb480873284f046ddadf977086
Death-Miner <vishalsinghgaira01@gmail.com>
2026-04-29 21:40:09 +0530

    This is the correct version of vector element pushing

--- basic/vector.cpp
+++ basic/vector.cpp
@@ -7,16 +7,23 @@ void display(vector <T> &v){
     typename vector <T> :: iterator itr;
 
     cout<<"The elements of the vector are"<<endl;
+    cout<<"[";
     
     for(auto itr=v.begin();itr!=v.end();itr++){
-        cout<<*itr<<" ";
+        cout<<*itr;
+        if(itr<(v.end()-1)){
+            cout<<",";
+            
+        }
+        
     }
-
+    cout<<"]";
 }
 int main(){
     int n;
-    float element;
+    int element;
     int choice;
+    
     cout<<"Enter the following what you want "<<endl;
     cout<<"1 if you want to do repetation of a single number "<<endl;
     cout<<"2 if you want to enter different different element "<<endl;
@@ -24,14 +31,28 @@ int main(){
     
     cout<<"Enter the choice you want to make "<<endl;
     cin>>choice;
-    
-    cout<<"Enter the total repetition "<<endl;
-    cin>>n;
-    cout<<"Enter the element you want to repeat"<<endl;
-    cin>>element;
-
-    vector <int> v1(n,element);
-    cout<<v1.size()<<endl;
-    display(v1);
+    if(choice==1){
+        
+        cout<<"Enter the total repetition "<<endl;
+        cin>>n;
+        cout<<"Enter the element you want to repeat"<<endl;
+        cin>>element;
+        vector <int> v1(n,element);
+        display(v1);
+    }
+    else if(choice==2) {
+        cout<<"Enter the total elements you wanna add ";
+        cin>>n;
+        vector <int> v1;
+        for(int i=0;i<n;i++){
+            cout<<"Enter the element "<<i+1<<endl;
+            cin>>element;
+            v1.push_back(element);
+        }
+        display(v1);
+    }
+    else{
+        cout<<"Already told working with others "<<endl;
+    }
 
 }
